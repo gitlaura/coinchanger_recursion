c0ae94cd9e72912c7aeee414d7a8ceb9c3b96678
Laura Rokita <getlaura@Lauras-MacBook-Air.local>
2014-05-28 12:57:38 -0500

    adding correct test directories

--- lib/coin_changer.rb
+++ lib/coin_changer.rb
@@ -11,10 +11,8 @@ def get_change(n, correct_change, coins)
  	if n >= coins[0]
  		i = n/coins[0]	
  		get_change(n -= (coins[0] * i), i.times { correct_change << coins[0]}, coins.shift)
- 		print "Correct Change: #{correct_change} Coins: #{coins}"
  	else
  		get_change(n, correct_change, coins.shift)
- 		print "Correct Change: #{correct_change} Coins: #{coins}"
  	end
 
 end
--- test/coin_changer_spec.rb
+++ test/coin_changer_spec.rb
@@ -1,10 +1,10 @@
 require '../lib/coin_changer.rb'
 
 describe "Coin Changer Kata" do
-	it	'changes 0' do
+	it	"changes 0" do
 		change_amount(0).should == []
 	end
-	it	'changes 1' do
+	it	"changes 1" do
 	 	change_amount(1).should == [1]
 	end
 end

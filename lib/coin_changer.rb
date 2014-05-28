74fc53f993fb1729e64ae7c4c99393f1e4a1890d
Laura Rokita <getlaura@Lauras-MacBook-Air.local>
2014-05-28 12:41:56 -0500

    updating test files

--- lib/coin_changer.rb
+++ lib/coin_changer.rb
@@ -18,13 +18,3 @@ def get_change(n, correct_change, coins)
  	end
 
 end
-
-
-describe "Coin Changer Kata" do
-	it	'changes 0' do
-		change_amount(0).should == []
-	end
-	it	'changes 1' do
-	 	change_amount(1).should == [1]
-	end
-end
